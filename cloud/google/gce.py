KMK-ONLINE/ansible-modules-core

commit c96fae008782b4c63cb180a0f096600dcffad012

To set external ip, libcloud needs a GCEAddress object

cloud/google/gce.py
@@ -377,7 +377,12 @@ def create_instances(module, gce, instance_names):
         except GoogleBaseError as e:
             module.fail_json(msg='Unexpected error attempting to get a static ip %s, error: %s' % (external_ip, e.value))
     else:
-        instance_external_ip = external_ip
+        # check if external_ip is an ip or a name
+        try:
+            socket.inet_aton(external_ip)
+            instance_external_ip = GCEAddress(id='unknown', name='unknown', address=external_ip, region='unknown', driver=gce)
+        except socket.error:
+            instance_external_ip = gce.ex_get_address(external_ip)
 
     new_instances = []
     changed = False

commit 42f5927cb946665dac0ef3c163cae00e06ebdd50

Fix ephemeral external_ip, broken by c96fae00

cloud/google/gce.py
@@ -394,6 +394,8 @@ def create_instances(module, gce, instance_names):
 
     if external_ip == "none":
         instance_external_ip = None
+    elif external_ip == 'ephemeral':
+        instance_external_ip = 'ephemeral'
     elif not isinstance(external_ip, basestring):
         try:
             if len(external_ip) != 0: